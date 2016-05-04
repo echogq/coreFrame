fd1ce2a19731f026371b608f3f8fc8b310ab6ddb
Apple <echogq@qq.com>
2016-05-04 10:41:54 +0800

    添加MaterialDesign 按钮定义实例

--- coreFrame/coreFrame/ViewController.swift
+++ coreFrame/coreFrame/ViewController.swift
@@ -7,14 +7,44 @@
 //
 
 import UIKit
+import MaterialKit
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var crashViewBtn: UIButton!
     
+    @IBOutlet weak var socketViewBtn: UIButton!
+    
+    @IBOutlet weak var tableViewBtn: UIButton!
+    
+    weak var mdBtn: MKButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.initViews()
+
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func initViews() -> Void {
+        
+        tableViewBtn.layer.borderColor = UIColor.blueColor().CGColor
+        
+        initMDBtn()
+        
+    }
+    /**
+     添加MaterialDesign按钮实例
+     */
+    func initMDBtn() -> Void {
+        mdBtn = MKButton(frame: CGRect(x: 20, y: 140, width: SCREEN_WIDTH-40, height: 35))
+        mdBtn.maskEnabled = true
+        mdBtn.cornerRadius = 5
+        mdBtn.backgroundAnimationEnabled = true
+        mdBtn.rippleEnabled = true
+        mdBtn.backgroundColor = UIColor.MKColor.LightBlue.P500
+        mdBtn.setTitle("MaterialDesign", forState: .Normal)
+        self.view.addSubview(mdBtn)
     }
 
     override func didReceiveMemoryWarning() {
